82eda75e07e5b9fd1156da19773dc9114e189835
vuceljedin1-dotcom <vucelj.edin1@gmail.com>
2025-12-28 12:47:19 +0100

    Update up.sh

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,1 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ -f ".env" ]; then
+  set -a
+  source .env
+  set +a
+fi
+
+./scripts/login_ngc.sh
+./scripts/pull_image.sh
+
+docker compose up -d
+docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}"
 
